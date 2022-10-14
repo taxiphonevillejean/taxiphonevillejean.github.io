4f106dc1d45075eb6c69462c486bb935f4203857
taxiphonevillejean <115285553+taxiphonevillejean@users.noreply.github.com>
2022-10-14 22:57:17 +0200

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -10,3 +10,23 @@ buttonBar.addEventListener("click", () => {
   menuContainer.classList.toggle("menu-transition");
   carouselZ.classList.toggle("zindex");
 });
+
+// *********************************************************
+
+const accueilButton = document.getElementById("accueil-button");
+const tarifsButton = document.getElementById("tarifs-button");
+const epicerieButton = document.getElementById("epicerie-button");
+const contactButton = document.getElementById("contact-button");
+
+accueilButton.addEventListener("click", () => {
+  location.href = "http://127.0.0.1:5500/index.html";
+});
+tarifsButton.addEventListener("click", () => {
+  location.href = "http://127.0.0.1:5500/index.html";
+});
+epicerieButton.addEventListener("click", () => {
+  location.href = "http://127.0.0.1:5500/html/Epicerie.html";
+});
+contactButton.addEventListener("click", () => {
+  location.href = "http://127.0.0.1:5500/html/Contacts.html";
+});
